36fb1f0f544c088f0d004081d00f6c7dff0e8382
Ivan Sushko <vaneksushko@gmail.com>
2023-06-22 22:24:17 +0300

    changes in item.py and test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_init(example):
+    assert example.name == "Смартфон"
+    assert example.price == 10000
+    assert example.quantity == 20
+
+
+def test_calculate_total_price(example):
+    assert example.calculate_total_price() == 200_000
+
+
+def test_apply_discount(example):
+    example.apply_discount()
+    assert example.price == 8000
+
